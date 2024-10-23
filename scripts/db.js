55ce0d7226c156cf957d0acf813b981f74187a77
David <146224919+davidlreg@users.noreply.github.com>
2024-10-23 13:23:49 +0200

    Use different book covers

--- scripts/db.js
+++ scripts/db.js
@@ -1,6 +1,6 @@
 let books = [
   {
-    name: "Die Geheimnisse des Ozeans",
+    name: "The secrets of the ocean",
     author: "Clara Meer",
     likes: 1250,
     liked: true,
@@ -11,32 +11,32 @@ let books = [
       {
         name: "Leser123",
         comment:
-          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+          "A fascinating adventure book that captivated me from the very first page.",
       },
       {
         name: "Bookworm84",
-        comment:
-          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+        comment: "A romantic story that touched my heart and made me think.",
       },
       {
         name: "FantasyFanatic",
-        comment:
-          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+        comment: "An exciting fantasy world that I found hard to put down.",
       },
       {
         name: "SciFiGuru",
         comment:
-          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+          "A clever science fiction novel with interesting time travel concepts and characters.",
       },
       {
         name: "NovelLover",
         comment:
-          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+          "A book that is full of magical surprises and that thrilled me.",
       },
     ],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_mystical_book_cover_for_The_Secrets_of_the_3.jpg",
   },
   {
-    name: "Der vergessene Pfad",
+    name: "The forgotten path",
     author: "Maximilian Schwarz",
     likes: 980,
     liked: false,
@@ -44,9 +44,11 @@ let books = [
     publishedYear: 2021,
     genre: "Fantasy",
     comments: [],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_mystical_book_cover_for_The_Forgotten_Path_3.jpg",
   },
   {
-    name: "Die Farben des Himmels",
+    name: "The colors of the sky",
     author: "Laura Blau",
     likes: 1520,
     liked: true,
@@ -57,32 +59,31 @@ let books = [
       {
         name: "LeserPeter",
         comment:
-          "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt.",
+          "The plot was captivating and the characters incredibly vivid.",
       },
       {
         name: "BookLover21",
-        comment:
-          "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat.",
+        comment: "A romantic masterpiece that touched and moved me deeply.",
       },
       {
         name: "FantasyNerd",
-        comment:
-          "Fantastische Welten und epische Abenteuer - genau mein Geschmack!",
+        comment: "Fantastic worlds and epic adventures - just my taste!",
       },
       {
         name: "SciFiEnthusiast",
         comment:
-          "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht.",
+          "The time travel elements were brilliant and made the story exciting.",
       },
       {
         name: "ReadingAddict",
-        comment:
-          "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat.",
+        comment: "An unforgettable book that took me on a magical journey.",
       },
     ],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_serene_and_dreamy_book_cover_for_The_Colors_2.jpg",
   },
   {
-    name: "Das Rätsel der Zeit",
+    name: "The riddle of time",
     author: "Alexander Weiss",
     likes: 750,
     liked: false,
@@ -93,17 +94,18 @@ let books = [
       {
         name: "BuchKenner",
         comment:
-          "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat.",
+          "An exciting adventure that captivated me right from the start.",
       },
       {
         name: "LeseWurm",
-        comment:
-          "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben.",
+        comment: "The love story was heart-wrenching and beautifully written.",
       },
     ],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_futuristic_book_cover_for_The_Riddle_of_Tim_2.jpg",
   },
   {
-    name: "Der letzte Wächter",
+    name: "The last guardian",
     author: "Sabine Grün",
     likes: 1300,
     liked: true,
@@ -111,9 +113,11 @@ let books = [
     publishedYear: 2017,
     genre: "Fantasy",
     comments: [],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_majestic_worn_leatherbound_book_cover_with_3.jpg",
   },
   {
-    name: "Im Schatten des Mondes",
+    name: "In the shadow of the moon",
     author: "Philipp Silber",
     likes: 890,
     liked: false,
@@ -124,17 +128,18 @@ let books = [
       {
         name: "BücherLiebhaber",
         comment:
-          "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd.",
+          "A magical journey through a fascinating fantasy world, absolutely captivating.",
       },
       {
         name: "Leseratte",
-        comment:
-          "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat.",
+        comment: "A gripping science fiction novel that made me think.",
       },
     ],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_futuristic_ethereal_book_cover_for_In_the_S_0.jpg",
   },
   {
-    name: "Jenseits der Sterne",
+    name: "Beyond the stars",
     author: "Oliver Schwarz",
     likes: 1450,
     liked: true,
@@ -145,12 +150,14 @@ let books = [
       {
         name: "Leser123",
         comment:
-          "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat.",
+          "A captivating adventure that swept me away from start to finish.",
       },
     ],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_futuristic_and_cosmic_book_cover_for_Beyond_2.jpg",
   },
   {
-    name: "Das verborgene Königreich",
+    name: "The hidden kingdom",
     author: "Elena Gold",
     likes: 920,
     liked: false,
@@ -161,12 +168,14 @@ let books = [
       {
         name: "Bookworm92",
         comment:
-          "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat.",
+          "A fascinating book that captivated me from the very first page.",
       },
     ],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_mystical_book_cover_for_The_Hidden_Kingdom_0.jpg",
   },
   {
-    name: "Liebe in Zeiten des Krieges",
+    name: "Love in times of war",
     author: "Emilia Rot",
     likes: 1800,
     liked: true,
@@ -177,18 +186,19 @@ let books = [
       {
         name: "Bibliophile23",
         comment:
-          "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen.",
+          "The fantasy world was so vivid that I could hardly put the book down.",
       },
       {
         name: "StorySeeker",
-        comment:
-          "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat.",
+        comment: "An incredibly touching love story that moved me deeply.",
       },
       {
         name: "SciFiExplorer",
         comment:
-          "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig.",
+          "Exciting visions of the future and interesting characters made this novel unique.",
       },
     ],
+    picture:
+      "./assets/img/Leonardo_Phoenix_A_hauntingly_beautiful_book_cover_for_the_rom_0.jpg",
   },
 ];
--- scripts/templates.js
+++ scripts/templates.js
@@ -4,40 +4,55 @@ function booksTemplate(i) {
       <div id="bookTitle-${i}">
         <h2>${books[i].name}</h2>
       </div>
+
       <div class="seperator"></div>
-      <img class="bookImg" src="img/book-1868068_1280.jpg" />
+
+      <img class="bookImg" id="bookImg-${i}" src="${
+    books[i].picture
+  }" alt="Book cover of ${books[i].name}" />
+
       <div class="seperator"></div>
+
       <div class="priceAndLikes">
+        <!-- Buchpreis -->
         <div class="bookPrice" id="bookPrice-${i}">
           <p id="price">${books[i].price.toFixed(2)}</p>€
         </div>
+
         <div class="bookLikes" id="bookLikes-${i}">
           <p id="likes-${i}">${books[i].likes}</p>
           <span id="likeHeart-${i}" class="material-symbols-outlined hearth">favorite</span>
         </div>
       </div>
+
       <table class="bookInfo">
+        <!-- Autor -->
         <tr id="author-${i}">
           <td>Author:</td>
           <td>${books[i].author}</td>
         </tr>
+
         <tr id="year-${i}">
-          <td>Erscheinungsjahr:</td>
+          <td>Year of publication:</td>
           <td>${books[i].publishedYear}</td>
         </tr>
+
         <tr id="genre-${i}">
           <td>Genre:</td>
           <td>${books[i].genre}</td>
         </tr>
       </table>
+
       <div class="seperator"></div>
+
       <div class="commentSectionWrapper">
         <h3 class="commentSectionHeadline">Kommentare:</h3>
         <div class="commentSection" id="commentSection-${i}">
         </div>
+
         <div class="commentSectionInput">
-          <input type="text" class="commentInput" id="commentInput-${i}" placeholder="Schreibe dein Kommentar ...">
-          <input class="commentButton" type="submit" value="Kommentieren" onclick="addComment(${i})">
+          <input type="text" class="commentInput" id="commentInput-${i}" placeholder="Write your comment ...">
+          <input class="commentButton" type="submit" value="Comment" onclick="addComment(${i})">
         </div>
       </div>
     </div> 
